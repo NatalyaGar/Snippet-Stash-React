50f8b84f7aef0248086c3789a12c4c497018ca59
Lisa Sprowls <lisasprowls@Lisas-MacBook-Pro.local>
2018-07-13 15:53:38 -0600

    updated files for Category Bar - not functional yet

--- client/src/components/CategoryBar/CategoryBar.js
+++ client/src/components/CategoryBar/CategoryBar.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import "./CategoryBar.css";
+import { Glyphicon, Button } from 'react-bootstrap';
 
 
 const CategoryBar = props => (
     <div className="col-2">
+        <div class="navbar-default sidebar" role="navigation">
+            <div class="sidebar-nav navbar-collapse">
+                <ul class="nav" id="side-menu">
+                    <li class="sidebar-search">
+                        <div class="input-group custom-search-form">
+                            <input type="text" class="form-control" placeholder="Search..." />
+                            <Button className="searchBtn">
+                                <Glyphicon glyph="search" />
+                            </Button>
+                        </div>
+                    
+                    </li>
+                    <li>
+                        <a href="#">
+                            <Glyphicon glyph="heart" /> My Snippets</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
         <ul>
             <li>HTML</li>
             <li>CSS</li>
@@ -11,4 +31,15 @@ const CategoryBar = props => (
     </div>
 );
 
+// export const CategoryBar = ({ children }) => {
+//     return (
+//       <div className="list-overflow-container">
+//         <ul className="list-group">
+//           {children}
+//         </ul>
+//       </div>
+//     );
+//   };
+
 export default CategoryBar
+
