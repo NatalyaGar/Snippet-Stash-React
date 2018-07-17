93e9d21ab2680adf37172db45b9d30f3ac8417d2
Melissa Gilman <melissadgilman@yahoo.com>
2018-07-16 21:25:49 -0600

    fixed connection issues

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+
 var routes = require('./routes');
+
 mongoose.Promise = require('bluebird');
-// mongoose.connect('mongodb://localhost/mern-crud', { useMongoClient: true, promiseLibrary: require('bluebird') })
-// .then(() =>  console.log('connection succesful'))
-// .catch((err) => console.error(err));
+mongoose.connect('mongodb://localhost/mern-crud', { useMongoClient: true, promiseLibrary: require('bluebird') })
+.then(() =>  console.log('connection succesful'))
+.catch((err) => console.error(err));
 
 var snippet = require('./routes/snippet');
 var app = express();
@@ -21,6 +23,7 @@ app.use(bodyParser.urlencoded({'extended':'false'}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/api/book', book);
+app.use('/api/snippet', snippet);
 
 // Add routes, both API and view
 app.use(routes);
@@ -33,15 +36,15 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
 
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 var categories = require('./categories');
+var snippets = require('./snippet')
 
 /* GET home page. */
 router.use('/api/categories', categories);
-// router.use('/api/snippets', snippets);
+router.use('/api/snippets', snippets);
 // router.use('/api/users', users);
 
 module.exports = router;
