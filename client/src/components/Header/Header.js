ed8e6dff8ec2b26ebf78b2e9bafa21d97286b7a9
Melissa Gilman <melissadgilman@yahoo.com>
2018-07-16 18:28:02 -0600

    header grabbing username is functional

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import "./Header.css";
+import ReactDOM from 'react-dom';
 // import { Glyphicon, } from 'react-bootstrap';
 import Headroom from 'react-headroom';
 // import "./Register.js"
 const user = {
     _id: '12345',
     name: 'Melissa',
-    isLoggedIn: false
+    
 }
+
+function UserGreeting(props) {
+    return <h1>Welcome Back {user.name}</h1>;
+}
+
+function GuestGreeting(props) {
+    return <h1>Welcome Guest</h1>;
+}
+
+function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn) {
+        return <UserGreeting />;
+    }
+    return <GuestGreeting />;
+}
+
+// ReactDOM.render(
+//     // Try changing to isLoggedIn={true}:
+//     <Greeting isLoggedIn={false} />,
+//     document.getElementById('root')
+// );
 
 
 //TODO: if user is logged in, pass in name instead of Guest
@@ -18,10 +41,13 @@ const Header = props => (
             <div className="row">
                 {/* <button classname="hamburger"> <Glyphicon glyph="glyphicon glyphicon-menu-hamburger" /> </button> */}
                 <h1 className="snippet-brand" href="/">
-                <span className="glyphicon glyphicon-scissors" aria-hidden="true"> </span>
+                    <span className="glyphicon glyphicon-scissors" aria-hidden="true"> </span>
                     SNIPPETstash </h1>
-                <p className="greeting welcome-message"> Welcome
-                <span> {user.isLoggedIn ? user.name : 'Guest'} </span>
+                <p className="greeting welcome-message"> 
+                
+                <span> <Greeting isLoggedIn={true} />,
+                    {/* {user.isLoggedIn ? user.name : 'Guest'}  */}
+                    </span>
                     {/* <span> {name}</span> */}
                     {/* <span> Guest </span> */}
                 </p>
