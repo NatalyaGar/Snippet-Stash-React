398b34730e104f94bc5abd1d125cb1168dc54bb4
Melissa Gilman <melissadgilman@yahoo.com>
2018-07-24 09:41:25 -0600

    heroku

--- app.js
+++ app.js
@@ -7,14 +7,16 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mern-crud";
 
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
-
 var routes = require('./routes');
-
 
 // mongoose.Promise = require('bluebird');
 // mongoose.connect('mongodb://localhost/mern-crud', { useMongoClient: true, promiseLibrary: require('bluebird') })
