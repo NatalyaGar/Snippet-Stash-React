7299077bbaeac7228dbd6f8a939485c0cf1e9065
Melissa Gilman <melissadgilman@yahoo.com>
2018-07-23 21:03:20 -0600

    reformatted colors

--- client/src/components/Show.js
+++ client/src/components/Show.js
@@ -32,33 +32,33 @@ class Show extends Component {
     return (
       <div className="container">
         <div className="panel panel-default">
-          <div className="panel-heading">
+          <div className="panel-heading" id="showCardHeading">
             <h3 className="panel-title">
               {this.state.snippet.title}
             </h3>
           </div>
           <div className="panel-body">
-            <h4><Link to="/"><span className="glyphicon glyphicon-arrow-left" aria-hidden="true"></span> Back to Snippet List </Link></h4>
+            <h4 ><Link className="backLink" to="/"><span className="glyphicon glyphicon-arrow-left" aria-hidden="true"></span> Back to Snippet List </Link></h4>
             <dl>
-              <dt>CATEGORY:</dt>
-              <dd>{this.state.snippet.category}</dd>
-              <dt>Author:</dt>
-              <dd>{this.state.snippet.author}</dd>
-              <dt>Description:</dt>
-              <dd>
+              <dt className="labels">CATEGORY:</dt>
+              <dd className="showData">{this.state.snippet.category}</dd>
+              <dt className="labels">AUTHOR:</dt>
+              <dd className="showData">{this.state.snippet.author}</dd>
+              <dt className="labels">DESCRIPTION:</dt>
+              <dd className="showData">
                 <textarea id="codeDescription" type="text" rows='11' cols="100" value={this.state.snippet.description} >
                 </textarea>
               </dd>
               
               <button type="button" className="btn btn-secondary clipboardBtn"><span>
-              <Clipboard component="a"  data-clipboard-text={this.state.snippet.description} button-title="Click to copy the entire code"> 
+              <Clipboard component="a" className="clipboardBtn" data-clipboard-text={this.state.snippet.description} button-title="Click to copy the entire code"> 
                 copy to clipboard
               </Clipboard></span> </button>
-              <dt>Comment:</dt>
-              <dd>{this.state.snippet.comment}</dd>
+              <dt className="labels">COMMENT:</dt>
+              <dd className="showData">{this.state.snippet.comment}</dd>
             </dl>
-            <Link to={`/edit/${this.state.snippet._id}`} className="btn btn-success">Edit</Link>&nbsp;
-            <button onClick={this.delete.bind(this, this.state.snippet._id)} className="btn btn-danger">Delete</button>
+            <Link to={`/edit/${this.state.snippet._id}`} className="btn btn-success changeBtn">Edit</Link>&nbsp;
+            <button onClick={this.delete.bind(this, this.state.snippet._id)} className="btn btn-danger changeBtn">Delete</button>
           </div>
         </div>
       </div>
